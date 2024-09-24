4d71d6229c06e3ddfd1d03b9e7f8ff8305cd181f
Bruno Silva <brunosilvabrait@gmail.com>
2024-09-24 18:01:40 -0300

    create controllers, adjust routes

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -2,14 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return 'Ola, seja bem vindo';
-});
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
 
-Route::get('/sobre-nos', function () {
-    return 'Sobre-nós';
-});
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
 
-Route::get('/contato', function () {
-    return 'Contato';
-});
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
