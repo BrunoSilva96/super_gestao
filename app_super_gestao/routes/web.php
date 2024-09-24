91e98812b50dfe6cd3c9ed62068ec5dad70d2d29
Bruno Silva <brunosilvabrait@gmail.com>
2024-09-24 16:44:16 -0300

    create routes

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -2,17 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return 'Ola, seja bem vindo';
+});
+
+Route::get('/sobre-nos', function () {
+    return 'Sobre-nós';
+});
+
+Route::get('/contato', function () {
+    return 'Contato';
 });
