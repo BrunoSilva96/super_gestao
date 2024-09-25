75cf32cd3b56169a953c461a5f0ccb7086c614ad
Bruno Silva <brunosilvabrait@gmail.com>
2024-09-25 16:07:16 -0300

    link in views

--- app_super_gestao/resources/views/site/principal.blade.php
+++ app_super_gestao/resources/views/site/principal.blade.php
@@ -1,1 +1,13 @@
 <h3> Principal (view) - {{$nome}} - {{$categoria}} </h3>
+
+<ul>
+  <li>
+    <a href='/'>Principal</a>
+  </li>
+  <li>
+    <a href='/sobre-nos'>Sobre nós</a>
+  </li>
+  <li>
+    <a href='/contato'>Contato</a>
+  </li>
+</ul>
--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -3,8 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get(
-  '/{nome}/{categoria_id}', [\App\Http\Controllers\PrincipalController::class, 'principal']
-)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
+  '/', [\App\Http\Controllers\PrincipalController::class, 'principal']
+);
+
+// Route::get(
+//   uma ? apos o nome do parametro {nome?} o torna não obrigatorio.
+//   '/{nome}/{categoria_id}', [\App\Http\Controllers\PrincipalController::class, 'principal']
+// )->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
 
 Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
 
