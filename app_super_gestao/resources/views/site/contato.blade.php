7feeff939fb6e5939350821a31ebec7485c9a322
Bruno Silva <brunosilvabrait@gmail.com>
2024-09-27 19:27:15 -0300

    {{ $slot }}, params to components {{ $class }}

--- app_super_gestao/resources/views/site/contato.blade.php
+++ app_super_gestao/resources/views/site/contato.blade.php
@@ -11,7 +11,9 @@
 
 		<div class="informacao-pagina">
 			<div class="contato-principal">
-				@component('site.layouts._components.form_contato')
+				@component('site.layouts._components.form_contato', ['classe' => 'borda-preta'])
+				<p>Nossa eequipe vai analisar e entrar em contato.</p>
+				<p>tempo médio de resposta em 24h.</p>
 				@endcomponent
 			</div>
 		</div>  
