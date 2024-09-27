76e061828203abbab2750a9e987579d186feb1cc
Bruno Silva <brunosilvabrait@gmail.com>
2024-09-27 18:28:26 -0300

    @include

--- app_super_gestao/app/Http/Controllers/PrincipalController.php
+++ app_super_gestao/app/Http/Controllers/PrincipalController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class PrincipalController extends Controller
 {
     public function principal(string $nome = 'Visitante',int $categoria_id = 1){
-        return view('site.principal', ['nome' => $nome, 'categoria' => $categoria_id]);
+        return view('site.principal', ['nome' => $nome, 'categoria' => $categoria_id, 'home' => 'Home']);
         //$dados = ['nome' => $nome, 'categoria' => $categoria_id];
         // Colcoar dentro de um array e passa no return
         //return view('siteprincipal', $dados)
