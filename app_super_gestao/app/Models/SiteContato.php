44502756e723498bba36ca468c4cfb5cacd88e1b
Bruno Silva <brunosilvabrait@gmail.com>
2024-10-11 16:35:09 -0300

    use method old and config fillable

--- app_super_gestao/app/Models/SiteContato.php
+++ app_super_gestao/app/Models/SiteContato.php
@@ -11,6 +11,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class SiteContato extends Model
 {
+    protected $fillable = ['nome', 'telefone', 'email', 'motivo_contato', 'mensagem'];
+    
     use HasFactory;
 
 }
