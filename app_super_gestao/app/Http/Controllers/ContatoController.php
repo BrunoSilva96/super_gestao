9cf9943c0577ce05edf01632fc2f7f3ccd7432c6
Bruno Silva <brunosilvabrait@gmail.com>
2024-10-11 16:34:49 -0300

    config request and direction routes to salvar

--- app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -3,12 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\SiteContato;
 
 class ContatoController extends Controller
 {
     public function contato(Request $request){
 
+       
+
+        // $contato->nome = $request->input('nome');
+        // $contato->telefone = $request->input('telefone');
+        // $contato->email = $request->input('email');
+        // $contato->motivo_contato = $request->input('motivo_contato');
+        // $contato->mensagem = $request->input('mensagem');
+
+        // $contato->save();
+
+
         
-        return view('site.contato');
+        return view('site.contato', ['titulo' => 'Contato teste']);
+    }
+
+    public function salvar(Request $request){
+
+      $request->validate([
+        'nome' => 'required|min:3|max:40',
+        'telefone' => 'required',
+        'email' => 'required',
+        'motivo_contato' => 'required',
+        'mensagem' => 'required|max:2000'
+      ]);
+      
+      SiteContato::create($request->all());
     }
 }
