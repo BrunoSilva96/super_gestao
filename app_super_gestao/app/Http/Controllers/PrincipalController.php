d95f4b37fc4383c046b87b6ecce5d34159bd3a80
Bruno Silva <brunosilvabrait@gmail.com>
2024-10-14 16:30:45 -0300

    refatoring site_contatos nad motivo_contato

--- app_super_gestao/app/Http/Controllers/PrincipalController.php
+++ app_super_gestao/app/Http/Controllers/PrincipalController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\MotivoContato;
 
 class PrincipalController extends Controller
 {
     public function principal(string $nome = 'Visitante',int $categoria_id = 1){
-        return view('site.principal', ['nome' => $nome, 'categoria' => $categoria_id, 'home' => 'Home']);
+        $motivo_contatos = MotivoContato::all();
+
+        return view('site.principal', ['motivo_contatos' => $motivo_contatos]);
         //$dados = ['nome' => $nome, 'categoria' => $categoria_id];
         // Colcoar dentro de um array e passa no return
         //return view('siteprincipal', $dados)
